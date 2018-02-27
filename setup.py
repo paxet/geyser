1be0863fe1558c4c8d088ab11bde99c8682f8a35
Javier Caballero <jcaballero@empretal.com>
2018-02-27 15:03:57 +0100

    Base for the project. Linked travis and heroku

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='geyser',
+    packages=['geyser'],
+    version='0.0.1a',
+    description='Gestión y seguimiento rápido para desarrollos informáticos',
+    url='https://github.com/paxet/geyser',
+)
